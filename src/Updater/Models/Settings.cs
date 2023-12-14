4991eb76edb23c37d8b298d7b037ac76fa267bd1
John <TPgMNMKQLre897@rs.ru>
2023-12-14 20:30:42 +0300

    Добавлен сервис получения обновлений

---
+++ src/Updater/Models/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.Models
+{
+	internal class Settings
+	{
+		public string MainServer { get; set; }
+		public string SecondServer { get; set; }
+		public string ProxyServer { get; set; }
+		public string ProxyPort { get; set; }
+		public string ProxyUserName { get; set; }
+		public string ProxyPassword { get; set; }
+		public string UpdateMode { get; set; }
+	}
+}
---
+++ src/Updater/Services/UpdateService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.Services
+{
+	public class UpdateService
+	{
+		/// <summary>
+		/// Папка в которую скачиваются обновления.
+		/// </summary>
+		private const string tempDirPath = "TempUpdate";
+
+		public string LastError { get; private set; }
+
+		/// <summary>
+		/// Читает настройки.
+		/// </summary>
+		public void ReadSettings()
+		{
+
+		}
+
+		/// <summary>
+		/// Очистка временной папки.
+		/// </summary>
+		/// <returns></returns>
+		public bool ClearTempDir()
+		{
+			if (Directory.Exists(tempDirPath))
+				try
+				{
+					Directory.Delete(tempDirPath, true);
+				}
+				catch (Exception ex)
+				{
+					LastError = ex.Message;
+					return false;
+				}
+
+			if (!Directory.Exists(tempDirPath))
+			{
+				Directory.CreateDirectory(tempDirPath);
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Получает информацию с сервера о текущих обновлениях.
+		/// </summary>
+		public void GetUpdateInfo()
+		{
+
+		}
+	}
+}
